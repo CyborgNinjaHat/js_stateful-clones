a8d477f8c773f3d4d1691effe279776843774928
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-04-01 18:52:49 +0000

    feat: implement state transformation

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,52 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
-}
+const transformStateWithClones = (state, actions) => {
+  if (actions.length === 0) {
+    return [];
+  }
+
+  const stateSequence = [];
+
+  const currentState = structuredClone(state);
+
+  for (const action of actions) {
+    const { type } = action;
+
+    if (type === 'addProperties') {
+      addProperties(currentState, action.extraData);
+    }
+
+    if (type === 'removeProperties') {
+      removeProperties(currentState, action.keysToRemove);
+    }
+
+    if (type === 'clear') {
+      clearProperties(currentState);
+    }
+
+    stateSequence.push(structuredClone(currentState));
+  }
+
+  return stateSequence;
+};
+
+const addProperties = (state, extraData) => {
+  Object.assign(state, extraData);
+};
+
+const removeProperties = (state, keysToRemove) => {
+  for (const key of keysToRemove) {
+    if (Object.hasOwn(state, key)) {
+      delete state[key];
+    }
+  }
+};
+
+const clearProperties = (state) => {
+  for (const key in state) {
+    delete state[key];
+  }
+};
 
 module.exports = transformStateWithClones;
